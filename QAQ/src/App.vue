bc9773163b769d95d7295a1f71b65e10be14d863
yyccQQu <643929860@qq.com>
2018-02-25 17:01:48 +0800

    add QAQ

---
+++ QAQ/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+import vHeader from './components/public/header'
+export default {
+  name: 'app',
+  components: {
+    vHeader
+  }
+}
+</script>
+
+<style lang="scss">
+@import "style/app";
+</style>
---
+++ QAQ/src/components/public/header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    <router-link to="/">问卷管理</router-link>
+    <router-link to="/">我的问卷</router-link>
+  </div>
+</template>
+
+<script>
+ 
+  export default {
+    name: 'header'
+  }
+</script>
+
+<style lang="scss" scoped>
+@import '../../style/header';
+</style>
